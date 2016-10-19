8ad15b58fbf315df47ba9359d68be9619e1bcbee
ChengruiZhu <cz8@illinois,edu>
2016-10-18 20:48:24 -0500

    models

--- moviefun/insert_all.py
+++ moviefun/insert_all.py
@@ -2,18 +2,17 @@
 
 from django.http import HttpResponse
 from moviefun.models import *
+import urllib2
+import json
 
 def insert(request):
-	dic={'movie_name':[], 
-	'year':[], 
-	'cast':[],
-	'rating':[],
-	'genre':[],
-	'director':[],
-	'city':[],
-	'country':[],
-	'title':[],
-	'time':[],
-	'season_number':[],
-	'episode_number':[]}
+for i in range(3603346, 3603456):
 
+        req = urllib2.Request('http://www.omdbapi.com/?i=tt' + str(i)+'&plot=short&r=json')
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+
+        parsed_json = json.loads(the_page)
+        # print(parsed_json)
+        if parsed_json['Response'] == 'True':
+               # print(parsed_json)
--- moviefun/models.py
+++ moviefun/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.IntegerField(default=1900)
-    cast = models.CharField(max_length=200)
-    rating = models.FloatField(default=0.0)
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
+    actors = models.CharField(max_length=200)
+    imdbrating = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
-    cover = models.CharField(max_length=200)
+    poster = models.CharField(max_length=500)
     director = models.CharField(max_length=200)
+    writer = models.CharField(max_length=200)
+    language = models.CharField(max_length=200)
+    runtime = models.CharField(max_length=200)
+    rated = models.CharField(max_length=200)
+    imdbid = models.CharField(max_length=200)
 
 class Loc(models.Model):
     city = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
-    longitude = models.FloatField(default=0.0)
-    latitude = models.FloatField(default=0.0)
+    longitude = models.CharField(max_length=200)
+    latitude = models.CharField(max_length=200)
 
 class MovieLocR(models.Model):
     loc_id = models.ForeignKey(Movie, related_name='loc_id', on_delete=models.CASCADE)
@@ -26,8 +31,8 @@ class RecomR(models.Model):
 class TVPlay(models.Model):
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    time = models.DateTimeField()
-    season_number = models.IntegerField(default=0)
-    episode_number = models.IntegerField(default=0)
+    time = models.CharField(max_length=200)
+    season_number = models.CharField(max_length=200)
+    episode_number = models.CharField(max_length=200)
 
 # Create your models here.
