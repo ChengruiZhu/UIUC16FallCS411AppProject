eb9d7af4c17afb0029452c3b76d4f0ddd480f763
ChengruiZhu <cz8@illinois.edu>
2016-10-18 22:13:41 -0500

    test

--- moviefun/insert_all.py
+++ moviefun/insert_all.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
-from moviefun.models import *
-import urllib2
 import json
+import urllib2
+
+from django.http import HttpResponse
+
+from moviefun.models import *
 
 def insert(request):
 for i in range(3603346, 3603456):
@@ -15,4 +17,38 @@ for i in range(3603346, 3603456):
         parsed_json = json.loads(the_page)
         # print(parsed_json)
         if parsed_json['Response'] == 'True':
-               # print(parsed_json)
+                t1 = Movie(title = parsed_json['Title'],
+                        year = parsed_json['Year'],
+                        rated = parsed_json['Rated'],
+                        released = parsed_json['Released'],
+                        runtime = parsed_json['Runtime'],
+                        genre = parsed_json['Genre'],
+                        director = parsed_json['Director'],
+                        writer = parsed_json['Writer'],
+                        actors = parsed_json['Actors'],
+                        plot = parsed_json['Plot'],
+                        language = parsed_json['Language'],
+                        awards = parsed_json['Awards'],
+                        poster = parsed_json['Poster'],
+                        metascore = parsed_json['Metascore'],
+                        imdbrating = parsed_json['imdbRating'],
+                        imdbvotes = parsed_json['imdbVotes'],
+                        imdbid = parsed_json['imdbID'],
+                        type = parsed_json['Type'])
+                t1.save()
+
+                if parsed_json['Type'] == 'episode':
+                        t2 = TVPlay(movie_id = t1,
+                                season = parsed_json['Season'],
+                                episode = parsed_json['Episode'],
+                                seriesid = parsed_json['seriesID'])
+
+                        t2.save()
+
+                if parsed_json['Type'] == 'series':
+                        t3 = TVSeries(movie_id = t1,
+                                totalseasons = parsed_json['totalSeasons'])
+                        t3.save()
+
+                     # print(parsed_json)
+        return HttpResponse("<p>数据添加成功！</p>")
