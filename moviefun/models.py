c43742c3635be1a5485d9db923724683ec3b4896
ChengruiZhu <cz8@illinois.edu>
2016-10-18 22:14:17 -0500

    test

--- moviefun/models.py
+++ moviefun/models.py
@@ -3,16 +3,22 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     year = models.CharField(max_length=200)
-    actors = models.CharField(max_length=200)
-    imdbrating = models.CharField(max_length=200)
+    rated = models.CharField(max_length=200)
+    released = models.CharField(max_length=200)
+    runtime = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
-    poster = models.CharField(max_length=500)
     director = models.CharField(max_length=200)
     writer = models.CharField(max_length=200)
+    actors = models.CharField(max_length=200)
+    plot = models.CharField(max_length=500)
     language = models.CharField(max_length=200)
-    runtime = models.CharField(max_length=200)
-    rated = models.CharField(max_length=200)
+    awards = models.CharField(max_length=200)
+    poster = models.CharField(max_length=200)
+    metascore = models.CharField(max_length=200)
+    imdbrating = models.CharField(max_length=200)
+    imdbvotes = models.CharField(max_length=200)
     imdbid = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
 
 class Loc(models.Model):
     city = models.CharField(max_length=200)
@@ -20,7 +26,7 @@ class Loc(models.Model):
     longitude = models.CharField(max_length=200)
     latitude = models.CharField(max_length=200)
 
-class MovieLocR(models.Model): 
+class MovieLocR(models.Model):
     loc_id = models.ForeignKey(Movie, related_name='loc_id', on_delete=models.CASCADE)
     movie_id = models.ForeignKey(Movie, related_name='movie_id', on_delete=models.CASCADE)
 
@@ -30,9 +36,11 @@ class RecomR(models.Model):
 
 class TVPlay(models.Model):
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    time = models.CharField(max_length=200)
-    season_number = models.CharField(max_length=200)
-    episode_number = models.CharField(max_length=200)
+    season = models.CharField(max_length=200)
+    episode = models.CharField(max_length=200)
+    seriesid = models.CharField(max_length=200)
 
+class TVSeries(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    totalseasons = models.CharField(max_length=200)
 # Create your models here.
