41ac7a6d27b2d86d63726ad0858c16ed88187e70
Suyang Chen <schen154@illinois.edu>
2016-10-23 10:42:25 -0500

    Update get_loc.py

--- get_loc.py
+++ get_loc.py
@@ -1,16 +1,17 @@
 from imdb import IMDb
-
+import re
 def getLocation(id):
         imdb_tool = IMDb()
         location= imdb_tool.get_movie_locations(id)
+
+#       for a in location['data']['locations']:
+#               print(a)
         loc = location['data']
-        
         if not loc:
                 return ''
-                
         loc = loc['locations'][0] #only get first location in the list
         loc = loc.split('(')[0] #get rid of everythign after '('
-
+        loc = re.sub('[^a-zA-Z\d\s,]', '', loc)#regex remove banned chars
         return loc
 
 #nice try
