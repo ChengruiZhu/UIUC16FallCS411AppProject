79f3df06dde02d2389255347c2a0a4ed1301bb6e
ChengruiZhu <cz8@illinois.edu>
2016-11-10 21:36:19 -0600

    test

--- moviefun/urls.py
+++ moviefun/urls.py
@@ -13,7 +13,9 @@ from . import insert_ll
 #from . import delete_all
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<lat_1>\f+&P<lat_2>\f+&P<log_1>\f+&P<log_2>\f+)/$',
+    # url(r'^post/(?P<lat_1>\f+&P<lat_2>\f+&P<log_1>\f+&P<log_2>\f+)/$',
+    #     views.post_detail, name='post_detail'),
+    url(r'^post/(?P<id>\d+)/$',
         views.post_detail, name='post_detail'),
     #url(r'test',testdb.testdb),
     url(r'insert/',insert.insert),
