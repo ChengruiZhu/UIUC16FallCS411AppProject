693f2bbcf0d7eaf051d52c57483e3d9f322098d1
ChengruiZhu <cz8@illinois.edu>
2016-11-10 22:34:39 -0600

    again

--- moviefun/views.py
+++ moviefun/views.py
@@ -32,39 +32,39 @@ def findMovie(list, long_min, long_max, la_min, la_max):
     return l
 
 
-# def post_detail(request, lat_1, lat_2, log_1, log_2):
-#     log_max = float(log_1) if float(log_1) > float(log_2) else float(log_2)
-#     log_min = float(log_1) if float(log_1) < float(log_2) else float(log_2)
-#     lat_max = float(lat_1) if float(lat_1) > float(lat_2) else float(lat_2)
-#     lat_min = float(lat_1) if float(lat_1) < float(lat_2) else float(lat_2)
-#
-#     list = Loc.objects.all()
-#
-#     l = []
-#     set = MovieLocR.objects.none()
-#     for var in list:
-#         if var.longitude != "N/A":
-#             if float(var.longitude) > log_min and float(var.longitude) < log_max \
-#                     and float(var.latitude) > lat_min and float(var.latitude) < lat_max:
-#                 l.append(var)
-#
-#     log_dist = (log_max-log_min)/log_num
-#     lat_dist = (lat_max-lat_min)/lat_num
-#     for i in range(log_num):
-#         for j in range(lat_num):
-#             set = set | findMovie(l, log_min+i*log_dist,
-#                                   log_min+(i+1)*log_dist,
-#                                   lat_min+j*lat_dist,
-#                                   lat_min+(j+1)*lat_dist)
-#     str = ''
-#     for var in set:
-#         str = str + var.imdbid + ' '
-#     return HttpResponse("<p>" + str + "</p>")
-
 def post_detail(request, lat_1, lat_2, log_1, log_2):
-    if request.method ==  'GET':
-        return HttpResponse("<p>" + str(lat_1) +' '+ str(lat_2) +' '+ str(log_1) +' '+ str(log_2) + "</p>")
-    elif request.method ==  'POST':
-        return HttpResponse("<p>" + str(lat_1) +' '+ str(log_1) +' '+ str(log_2) + "</p>")
+    log_max = float(log_1) if float(log_1) > float(log_2) else float(log_2)
+    log_min = float(log_1) if float(log_1) < float(log_2) else float(log_2)
+    lat_max = float(lat_1) if float(lat_1) > float(lat_2) else float(lat_2)
+    lat_min = float(lat_1) if float(lat_1) < float(lat_2) else float(lat_2)
+
+    list = Loc.objects.all()
+
+    l = []
+    set = MovieLocR.objects.none()
+    for var in list:
+        if var.longitude != "N/A":
+            if float(var.longitude) > log_min and float(var.longitude) < log_max \
+                    and float(var.latitude) > lat_min and float(var.latitude) < lat_max:
+                l.append(var)
+
+    log_dist = (log_max-log_min)/log_num
+    lat_dist = (lat_max-lat_min)/lat_num
+    for i in range(log_num):
+        for j in range(lat_num):
+            set = set | findMovie(l, log_min+i*log_dist,
+                                  log_min+(i+1)*log_dist,
+                                  lat_min+j*lat_dist,
+                                  lat_min+(j+1)*lat_dist)
+    str = ''
+    for var in set:
+        str = str + var.imdbid + ' '
+    return HttpResponse("<p>" + str + "</p>")
+
+# def post_detail(request, lat_1, lat_2, log_1, log_2):
+#     if request.method ==  'GET':
+#         return HttpResponse("<p>" + str(lat_1) +' '+ str(lat_2) +' '+ str(log_1) +' '+ str(log_2) + "</p>")
+#     elif request.method ==  'POST':
+#         return HttpResponse("<p>" + str(lat_1) +' '+ str(log_1) +' '+ str(log_2) + "</p>")
 
 
